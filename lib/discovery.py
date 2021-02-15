22ce1677a84aa3889ccb9d69da95b6d6d7dd54f2
Montellese <sascha.montellese@gmail.com>
2021-02-15 22:50:20 +0100

    ~ replace custom PMS network discovery with plexapi.gdm.GDM

--- lib/discovery.py
+++ lib/discovery.py
@@ -7,13 +7,13 @@
 #
 from __future__ import annotations  # Necessary for forward reference annotations (return of fromString method)
 import time
-import socket
-import struct
 
 from six import iteritems
 
 import xbmc  # pylint: disable=import-error
 import xbmcmediaimport  # pylint: disable=import-error
+
+from plexapi.gdm import GDM
 
 from lib.monitor import Monitor
 from lib.settings import ProviderSettings
@@ -43,44 +43,38 @@ class PlexServer():
         return self.registered and self.lastseen + timeoutS < time.time()
 
     @staticmethod
-    def fromString(data: str, ip: str) -> PlexServer:
+    def fromData(response: dict) -> PlexServer:
         """Construct and return a PlexServer object from discovery response data
 
-        :param data: Response from a discovery message
-        :type data: str
-        :param ip: IP address the response was received from
-        :type ip: str
+        :param response: Response from a discovery message
+        :type response: dict
         :return: Constructed PLexServer object
         :rtype: :class:`PlexServer`
         """
-        Separator = b':'
-        ServerPropertyResourceIdentifier = b'Resource-Identifier'
-        ServerPropertyName = b'Name'
-        ServerPropertyPort = b'Port'
+        AttributeData = 'data'
+        AttributeFrom = 'from'
+        AttributeResourceIdentifier = 'Resource-Identifier'
+        AttributeName = 'Name'
+        AttributePort = 'Port'
 
-        if not data:
+        # make sure the response is valid
+        if not {AttributeData, AttributeFrom}.issubset(response.keys()):
             return None
-        if not ip:
+
+        # make sure the data part of the response is valid
+        data = response[AttributeData]
+        if not {AttributeResourceIdentifier, AttributeName, AttributePort}.issubset(data.keys()):
             return None
 
-        identifier = None
-        name = None
-        port = 32400
+        # make sure the from part of the response is valid
+        from_data = response[AttributeFrom]
+        if not isinstance(from_data, tuple) or len(from_data) != 2:
+            return None
 
-        for line in data.splitlines():
-            lineParts = [linePart.strip() for linePart in line.split(Separator)]
-            if len(lineParts) > 2:
-                lineParts = [lineParts[0], Separator.join(lineParts[1:])]
-            if len(lineParts) != 2:
-                continue
-
-            serverProperty, serverPropertyValue = lineParts
-            if serverProperty == ServerPropertyResourceIdentifier:
-                identifier = serverPropertyValue.decode('utf-8')
-            elif serverProperty == ServerPropertyName:
-                name = serverPropertyValue.decode('utf-8')
-            elif serverProperty == ServerPropertyPort:
-                port = int(serverPropertyValue)
+        identifier = data[AttributeResourceIdentifier]
+        name = data[AttributeName]
+        port = int(data[AttributePort])
+        ip = from_data[0]
 
         if not identifier or not name or port <= 0 or port > 65535:
             return None
@@ -92,47 +86,27 @@ class PlexServer():
         server.registered = False
         server.lastseen = time.time()
 
-        if not server.id or not server.name or not server.address:
-            return None
-
         return server
 
 
 class DiscoveryService:
     """Class that handles discovery of Plex servers on the local network"""
-    DiscoveryAddress = '239.0.0.250'
-    DiscoveryPort = 32414
-    DiscoveryMessage = b'M-SEARCH * HTTP/1.1\r\n'
-    DiscoveryTimeoutS = 1.0
-    DiscoveryResponsePort = 32412
-    DiscoveryResponse = b'200 OK'
 
     def __init__(self):
         self._monitor = Monitor()
-        self._sock = None
+        self._gdm = GDM()
         self._servers = {}
 
         self._start()
 
     def _discover(self):
-        """Sends discovery multicast message on the existing socket and waits for resposne, adding server if found"""
-        # broadcast the discovery message
-        self._sock.sendto(self.DiscoveryMessage, (self.DiscoveryAddress, self.DiscoveryPort))
+        """Uses plexapi's GDM class to discover servers"""
+        servers = self._gdm.find_by_content_type('plex/media-server')
 
-        # try to receive an answer
-        data = None
-        address = None
-        try:
-            (data, address) = self._sock.recvfrom(1024)
-        except socket.timeout:
-            return
-
-        if not address or not data or self.DiscoveryResponse not in data:
-            return
-
-        server = PlexServer.fromString(data, address[0])
-        if server is not None:
-            self._addServer(server)
+        for server in servers:
+            plexServer = PlexServer.fromData(server)
+            if plexServer:
+                self._addServer(plexServer)
 
     def _addServer(self, server: PlexServer):
         """Add a discovered PMS server as a MediaProvider to the Kodi mediaimport system
@@ -204,13 +178,6 @@ class DiscoveryService:
         """Start the discovery and registration process"""
         log('Looking for Plex Media Servers...')
 
-        # setup the UDP broadcast socket
-        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        ttl = struct.pack('b', 1)
-        self._sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-        self._sock.settimeout(self.DiscoveryTimeoutS)
-
         while not self._monitor.abortRequested():
             # try to discover Plex media servers
             self._discover()
@@ -220,5 +187,3 @@ class DiscoveryService:
 
             if self._monitor.waitForAbort(1):
                 break
-
-        self._sock.close()
